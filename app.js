281580dd745d25b87d24484048b266b5e47f0728
Emil Alasgarov <Emil.Alasgarov@VTBAZE.local>
2017-03-15 12:17:18 +0400

    started work with async

--- app.js
+++ app.js
@@ -3,20 +3,45 @@ tokenizer = new natural.WordTokenizer(),
 PorterStemmerRu = require('./node_modules/natural/lib/natural/stemmers/porter_stemmer_ru'),
 nodehun = require('nodehun'),
 fs = require('fs'),
-affbuf = fs.readFileSync('/home/datafile4/host/spellcheckers/dictionaries/Russian.aff'),
-dictbuf = fs.readFileSync('/home/datafile4/host/spellcheckers/dictionaries/Russian.dic'),
-trainFile = fs.readFileSync('/home/datafile4/host/data/data.csv');
+async = require('async'),
+affbuf = fs.readFileSync('C:\\Users\\emil.alasgarov\\SharedVmware\\spellcheckers\\dictionaries\\Russian.aff'),
+dictbuf = fs.readFileSync('C:\\Users\\emil.alasgarov\\SharedVmware\\spellcheckers\\dictionaries\\Russian.dic'),
+trainFile = fs.readFileSync('C:\\Users\\emil.alasgarov\\SharedVmware\\data\\data.csv');
 stopwords_ru = require('stopwords-ru');
 var dict = new nodehun(affbuf, dictbuf);
 var classifier = new natural.BayesClassifier();
+/*trainFile.toString().split(/\n/).forEach(function (line) {
+var string = tokenizer.tokenize(line.replace('ё','e'));
+//var sentence = tokenizer.tokenize(line[0]);
+//var string = line.split(",");
+var sentence = string.slice(0, -1);
+var sentence_class = string[string.length - 1];
+sentence.forEach(function (word, index, array) {
+dict.spellSuggest(word, function (err, correct, suggestion, origWord) {
+if (!err) {
+if (correct) {
+array[index] = origWord;
+} else if (suggestion != null) {
+array[index] = suggestion;
+}
+console.log("orig: " + word + " ,suggested: " + array[index]);
+}
+});
+});
+sentence.forEach(function (word, index, array) {
+console.log("PING!");
+if (contains(word, stopwords_ru)) {
+array.splice(index, 1);
+}
+});
+console.log("S: " + sentence + " C: " + sentence_class);
+//classifier.addDocument(sentence, sentence_class);
+});*/
 trainFile.toString().split(/\n/).forEach(function (line) {
-	var string = tokenizer.tokenize(line);
-	//var sentence = tokenizer.tokenize(line[0]);
-	//var string = line.split(",");
-	var sentence = string.slice(0, -1)
+	var string = tokenizer.tokenize(line.replace('ё', 'e'));
+	var sentence = string.slice(0, -1);
 	var sentence_class = string[string.length - 1];
-	//console.log("S: " + sentence + " C: " + sentence_class);
-	sentence.forEach(function (word, index, array) {
+	async.each(sentence, function (word, callback) {
 		dict.spellSuggest(word, function (err, correct, suggestion, origWord) {
 			if (!err) {
 				if (correct) {
@@ -24,20 +49,17 @@ trainFile.toString().split(/\n/).forEach(function (line) {
 				} else if (suggestion != null) {
 					array[index] = suggestion;
 				}
-				if(contains(array[index],stopwords_ru)){
-					array.splice(index,1);
-					console.log("PING!");
-				}
+				console.log("orig: " + word + " ,suggested: " + array[index]);
 			}
+			callback();
 		});
 	});
-	console.log("S: " + sentence + " C: " + sentence_class);
-	//classifier.addDocument(sentence, sentence_class);
-});
-//classifier.train();
-//classifier.save('classifier.json');
+}
+console.log("The End");
+	//classifier.train();
+	//classifier.save('classifier.json');
 
-function contains(word, stopwords_ru) {
+	function contains(word, stopwords_ru) {
 	var flag = false;
 	for (var i = 0; i < stopwords_ru.length; i++) {
 		if (stopwords_ru[i] == word) {
